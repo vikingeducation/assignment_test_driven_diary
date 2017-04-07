efce6878864ad98fa869003b1baa1603cb516722
deven1 <deven@sloan.mit.edu>
2017-04-07 16:39:25 -0400

    tested entry function with two options

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
-const Diary = require('./lib/diary');
+const Diary = require("./lib/diary");
 const diary = new Diary();
 
-diary.entry('I love Brad', Date.now());
-console.log(diary.entryItems);
+diary.entry("I love Brad", Date.now());
+
+console.log(diary._entries);
--- spec/diarySpec.js
+++ spec/diarySpec.js
@@ -1,20 +1,30 @@
-const Diary = require('../lib/diary');
+const Diary = require("../lib/diary");
 
-describe('Diary', () => {
+describe("Diary", () => {
   let diary;
 
-  beforeEach( () => {
+  beforeEach(() => {
     diary = new Diary();
   });
 
-  describe('.entry', () => {
-    it('creates a new diary entry item', () => {
-      diary.entry('Deven is so cool.', Date.parse("Mon, 25 Dec 1995 13:30:00 GMT"));
-      expect(diary.entryItems[0]).toEqual({
-        text: 'Deven is so cool.',
-        date: 819898200000
-      })
-    });
-  });
+  describe(".entry", () => {
+    it("creates a new diary entry item without date", () => {
+      diary.entry("Jerry is so cool.");
 
+      expect(diary._entries[0].text).toEqual("Jerry is so cool.");
+
+      //expect(diary._entries[0].date).toBeCloseTo(Date.now(), 0);
+    }); // end of it
+
+    it("creates a new diary entry item with date", () => {
+      diary.entry(
+        "Deven is so cool.",
+        Date.parse("Mon, 25 Dec 1995 13:30:00 GMT")
+      );
+      expect(diary._entries[0]).toEqual({
+        text: "Deven is so cool.",
+        date: 819898200000
+      });
+    }); // end of it
+  });
 });
