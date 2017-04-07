04da253ea6d5e307dc8af3f3571c5294805c8208
William Whitworth <william10charles@gmail.com>
2017-04-07 14:36:56 -0600

    refactor approach

--- specs/diary.spec.js
+++ specs/diary.spec.js
@@ -1,14 +1,15 @@
 describe("Diary", function() {
-  var diary = require("../lib/diary");
+  const Diary = require("../lib/diary");
+  var diary;
   beforeEach(function() {
-    diary.stored = ["this is the only entry"];
+    diary = new Diary();
   });
 
-  it("Two params entered in diary", function() {
-    expect(diary.entry("a", "b")).toEqual("a - b");
+  it("calling entry adds an entry to the users Diary", function() {
+    expect(diary.entry("a").length).toEqual(1);
   });
-  it("Single param entered in diary", function() {
-    expect(diary.entry("a")).toEqual(`a - ${Date.now()}`);
+  it("Calling entry() with single param will result in a return array length 1", function() {
+    expect(diary.entry("a", "adsf").length).toEqual(1);
   });
   xit("display single entry from diary", function() {
     expect(diary.entries()).toEqual();
