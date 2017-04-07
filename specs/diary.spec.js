9eb6f666ca6fa280a3e2cb3fdc1592575fa6149c
tim5046 <tim5046@gmail.com>
2017-04-07 15:09:28 -0700

    working on search

--- specs/diary.spec.js
+++ specs/diary.spec.js
@@ -6,8 +6,8 @@ describe("Diary", function() {
   });
 
   it("calling entry adds an entry to the users Diary", function() {
-    let entryLength = diary.entry("a").length;
-    expect(entryLength).toEqual(1);
+    let entry = diary.entry("a");
+    expect(entry.length).toEqual(1);
   });
   it("calling entry with a date adds an entry to the users Diary", function() {
     let entriesLength = diary.entry("a", "adsf").length;
@@ -40,10 +40,42 @@ describe("Diary", function() {
       "I'm at Brad's window #yolo"
     ]);
   });
+
   it(".today returns a list of all entries written today", function() {
+    diary.entry("Another post today!");
     diary.entry("OMG. What have I done? #sorrynotsorry");
     diary.entry("OMG. What have I done? #sorrynotsorry", "aug-20-1999");
     let todaysEntry = diary.today();
-    expect(todaysEntry).toEqual(["OMG. What have I done? #sorrynotsorry"]);
+    expect(todaysEntry).toEqual([
+      "Another post today!",
+      "OMG. What have I done? #sorrynotsorry"
+    ]);
   });
+
+  it(".date returns a list of all entries written on the given date", function() {
+    diary.entry("Post 1");
+    diary.entry("Post 2", "1978-01-12");
+    diary.entry("Post 3", "2010-10-10");
+    diary.entry("Post 4", "2010-10-10");
+
+    let date = Date.parse("10/10/10");
+    expect(diary.date(date)).toEqual([
+      "Post 3",
+      "Post 4"
+    ]);
+  });
+
+  it(".search should return a list of all notes with the given string.", function() {
+    diary.entry("Today, Brad accidentally touched my hand in the hallway.");
+    diary.entry("Brad is a dreamboat.");
+    diary.entry("My dad is sooo annoying.");
+
+    expect(diary.search('Brad')).toEqual([
+      "Today, Brad accidentally touched my hand in the hallway.",
+      "Brad is a dreamboat."
+    ]);
+  });
+
+
+  //
 });
