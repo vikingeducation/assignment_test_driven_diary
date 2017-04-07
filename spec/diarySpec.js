629ac699f04fb3d894dd9b3422d19f7376ef341e
Jerry Gao <gao@blackwright.com>
2017-04-07 17:28:00 -0400

    add tagging functions

--- spec/diarySpec.js
+++ spec/diarySpec.js
@@ -9,22 +9,43 @@ describe("Diary", () => {
 
   describe(".entry", () => {
     it("creates a new diary entry item without date", () => {
-      diary.entry("Jerry is so cool.");
+      diary.entry("Jerry is so cool");
 
-      expect(diary._entries[0].text).toEqual("Jerry is so cool.");
+      expect(diary._entries[0].text).toEqual("Jerry is so cool");
 
       //expect(diary._entries[0].date).toBeCloseTo(Date.now(), 0);
-    }); // end of it
+    });
 
     it("creates a new diary entry item with date", () => {
       diary.entry(
-        "Deven is so cool.",
+        "Deven is so cool",
         Date.parse("Mon, 25 Dec 1995 13:30:00 GMT")
       );
       expect(diary._entries[0]).toEqual({
-        text: "Deven is so cool.",
+        text: "Deven is so cool",
         date: 819898200000
       });
-    }); // end of it
+    });
+
+    it('does not create a tag for an entry without a hashtag', () => {
+      diary.entry('Jerry is so cool');
+      expect(diary._entries[0]).toBeUndefined();
+    });
+
+    it('creates a tag for an entry with a hashtag', () => {
+      diary.entry('Deven is so cool #vikings');
+      expect(diary._entries[0]).toEqual('vikings');
+    });
+
+    it('stores all the tags in the diary');
+  });
+
+  describe('.entries', () => {
+    it('returns all of the entries in a diary', () => {
+      diary.entry("Jerry is so cool");
+      diary.entry("Deven is so cool");
+      expect(diary.entries()[0].text).toEqual("Jerry is so cool");
+      expect(diary.entries()[1].text).toEqual("Deven is so cool");
+    });
   });
 });
