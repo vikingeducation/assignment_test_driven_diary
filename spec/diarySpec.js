46c272e7f6f2336ab853044fb31131aa4cf81f76
Will Timpson <will.timpson@gmail.com>
2017-07-28 16:52:52 -0400

    Basic reading and writing

--- spec/diarySpec.js
+++ spec/diarySpec.js
@@ -6,21 +6,24 @@ let diary;
 describe("diary", () => {
   it("should have a file to write to", function() {
     diary = new Diary("test.json");
-    expect(diary.filename).toEqual("../../data/test.json");
+    expect(diary.filename).toEqual("./data/test.json");
   });
   it("should return correct default file", function() {
     diary = new Diary();
-    expect(diary.filename).toEqual("../../data/diary.json");
+    expect(diary.filename).toEqual("./data/diary.json");
   });
 });
 
 describe("entry", () => {
   beforeEach(() => {
     diary = new Diary("test.json");
-    fs.writeFileSync("../data/test.json", "");
+    fs.writeFileSync("./data/test.json", "{}");
   });
 
   it("should add to the diary file", function() {
-    expect(diary.entry(""));
+    diary.entry("Brad is everything to me.");
+    let diaryFile = fs.readFileSync("./data/test.json", "utf8");
+    let diaryObj = JSON.parse(diaryFile);
+    expect(diaryObj.entries[0]).toEqual("Brad is everything to me.");
   });
 });
