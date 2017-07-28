307ada3fdb272766e4c74ceda2d61522dbdec184
Ian Halverson <halversoncode@gmail.com>
2017-07-28 13:01:58 -0400

    added our names

--- javascript/hello-world/hello-world.spec.js
+++
@@ -1,9 +0,0 @@
-var HelloWorld = require('./hello-world');
-
-describe('Hello World', function() {
-  var helloWorld = new HelloWorld();
-
-  it('says hello world', function() {
-    expect(helloWorld.hello()).toEqual('Hello, World!');
-  });
-});
