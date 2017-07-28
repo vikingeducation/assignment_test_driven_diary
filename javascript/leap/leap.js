0e3b8a4b32eaab8eb4089bba83f39cadd2c57807
Stephanie Barker <song_mage@yahoo.com>
2017-07-28 11:33:27 -0400

    first couple of exercism exercises

---
+++ javascript/leap/leap.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Year = function(input) {
+	this.year = input;
+};
+
+Year.prototype.isLeap = function() {
+	if (this.year % 4 === 0 && this.year % 100 !== 0) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+module.exports = Year;
+
+year = new Year(2016);
+
+console.log(year.year);
