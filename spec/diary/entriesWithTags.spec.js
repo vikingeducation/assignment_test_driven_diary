c027f010e1dbe982e248a6f2798fcb55299378d4
EdTriplett <EdTriplett@users.noreply.github.com>
2017-07-28 18:30:55 -0400

    todayish

--- spec/diary/entriesWithTags.spec.js
+++ spec/diary/entriesWithTags.spec.js
@@ -1,5 +1,16 @@
-describe("The plus operator", function() {
-	it("adds two numbers together", function() {
-		expect(3 + 3).toEqual(6);
-	});
+const { entriesWithTags } = require("../../lib/diary");
+const fs = require("fs");
+
+const file = "./diary/diary.txt";
+
+describe("The entriesWithTags method", function() {
+  beforeEach(() => {
+    fs.writeFileSync(file, "entry1 #yolo #running\nentry2 #chocolate\nentry3 #yolo");
+  });
+
+  it("should return a list of all entries with the parameter tag", function() {
+    let tagList = entriesWithTags('yolo');
+    let expectedArray = ["entry1 #yolo #running", "entry3 #yolo"];
+    expect(tagList).toEqual(expectedArray);
+  });
 });
