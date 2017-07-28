09c510fae27f4af434b66683b3c4c13628fed29f
Stephanie Barker <song_mage@yahoo.com>
2017-07-28 13:31:13 -0400

    creating testing suite

---
+++ spec/diary.spec.js
@@ -0,0 +1,75 @@
+describe("Diary", () => {
+	describe("diary loading", () => {
+		it("should load a diary from a file", () => {
+
+		});
+	});
+
+	describe("diary entry creation" () => {
+		it("should save an entry by id", () => {
+
+		});
+
+		it("should save all entries as strings", () => {
+
+		});
+
+		it("should save an entry by tags if it has any", () => {
+
+		});
+
+		it("should create new tags if they don't already exist", () => {
+
+		});
+
+		it("should save an entry by date", () => {
+
+		});
+
+		it("should save an entry by a user-specified date", () => {
+
+		});
+
+		it("should should not accept an invalid date", () => {
+
+		});
+
+		it("should create a new entry even if content and date are the same", () => {
+
+		});
+
+		it("should not save a blank entry", () => {
+
+		});
+	});
+
+	describe("diary searching", () => {
+		beforeEach(() => {
+			// write some data to a diary
+		});
+
+		it("should return a list of all entries", () => {
+
+		});
+
+		it("should return all entries with a certain tag", () => {
+
+		});
+
+		it("should return all tags that have been used", () => {
+
+		});
+
+		it("should return all entries from the current day", () => {
+
+		});
+
+		it("should return all entries from a given date", () => {
+
+		});
+
+		it("should return all entries containing a search string", () => {
+
+		});
+	});
+});
