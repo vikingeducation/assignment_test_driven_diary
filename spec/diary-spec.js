de8aaef9fa72e6cbfe451eadb3de57935005c820
Malyar Baron <malbaron0@gmail.com>
2017-08-15 17:58:38 -0400

    Additions of extra test for Diary and utils

--- spec/diary-spec.js
+++ spec/diary-spec.js
@@ -1,8 +1,10 @@
 const Diary = require("../src/diary");
+const moment = require('moment');
+
 
 describe('diary', () => {
     let diary
-    
+
     beforeEach(() => {
         diary = new Diary();
     });
@@ -15,8 +17,8 @@ describe('diary', () => {
         });
 
         it('should take second param date and store with message', () => {
-            let date = Date();
-            diary.entry("Writing first js tests today", date);
+            let date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+            diary.entry("Writing first js tests today", Date.now());
             expect(diary.entryList[0].timeStamp).toEqual(date);
         });
 
@@ -28,44 +30,46 @@ describe('diary', () => {
 
         it('should store anytags', () => {
             diary.entry("Writing first js tests today #jasmineTea");
-            expect(diary.entryList[0].tags[0]).toEqual('#jasmineTea');
+            expect(diary.entryList[0].tags[0]).toEqual('jasmineTea');
         });
 
     });
 
     describe('entries method', () => {
         it('should return all entries', () => {
-            diary.entry("test message 1");
+            diary.entry("test message 1 #hurricane");
             diary.entry("test message 2");
             diary.entry("test message 3");
 
             let entryTestMsgs = ["test message 1", "test message 2", "test message 3"];
 
-            expect(diary.entries).toEqual(entryTestMsgs);
+            expect(diary.entries()).toEqual(entryTestMsgs);
         });
     });
 
     describe('tags method', () => {
+        let testTagList;
+
         beforeEach(() => {
 
             diary.entry("test message 1 #jasmineTea");
             diary.entry("test message 2 #greenTea");
             diary.entry("test message 3 #blackTea");
             diary.entry("test message 4 #jasmineTea");
-            
 
-            let testTagList = ["jasmineTea", "greenTea", "blackTea"];
+
+            testTagList = ["jasmineTea", "greenTea", "blackTea"];
 
         });
 
         it('should return array of tags used in all entries w/o duplicates', () => {
-            expect(diary.tags).toEqual(testTagList);
+            expect(diary.tags()).toEqual(testTagList);
 
         });
     });
 
     describe('getting entries with tag', () => {
-        
+
         beforeEach(() => {
             diary.entry("test message 1 #jasmineTea");
             diary.entry("test message 2 #greenTea");
@@ -75,6 +79,52 @@ describe('diary', () => {
 
         it('should get all entries with a given tag', () => {
             expect(diary.entriesWithTag("jasmineTea")).toEqual(["test message 1", "test message 3"]);
+        });
+    });
+
+    describe('date method', () => {
+        let testDate;
+
+        beforeEach(() => {
+            testDate = Date.parse("10/10/10");
+            diary.entry("test message 1 #jasmineTea", testDate);
+            diary.entry("test message 2 #greenTea", testDate);
+            diary.entry("test message 3 #jasmineTea");
+
+        });
+
+
+        it('should return all entries with given date', () => {
+            expect(diary.date(testDate)).toEqual(["test message 1", "test message 2"]);
+        });
+
+
+    });
+
+    describe('today method', () => {
+        let testDate;
+
+        beforeEach(() => {
+            testDate = Date.now();
+            diary.entry("test message 1 #jasmineTea", Date.parse("Tue Aug 12 2017 11:51:30 GMT"));
+            diary.entry("test message 2 #greenTea", testDate);
+            diary.entry("test message 3 #jasmineTea");
+            diary.entry("test message 4 #allTea", Date.parse("Tue Aug 13 2017 1:51:30 GMT"));
+        });
+
+        it('should get all entries written today', () => {
+            expect(diary.today()).toEqual(["test message 2", "test message 3"]);
+        });
+    });
+
+    describe('search method', () => {
+        it('should return messages containing the keyword', () => {
+
+            diary.entry("test message 2 #greenTea");
+            diary.entry("test message 3 #jasmineTea");
+            diary.entry("Today I saw the sun for the first time #sun");
+
+            expect(diary.search("message")).toEqual(["test message 2", "test message 3"]);
         });
     });
 });
--- src/diary.js
+++ src/diary.js
@@ -1,34 +1,102 @@
+const moment = require('moment');
+const utils = require('../tools/utils');
+
 class Diary {
-    
+
     constructor() {
         this.entryList = [];
+        this.allTags = [];
     }
 
+    //Store message with correct timestamp and store tag if included in message
     entry(msg, time) {
 
-        if (!time) {
-            time = Date();
-        }
+        if (!time) time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+        else time = moment(time).format("dddd, MMMM Do YYYY, h:mm:ss a");
 
-        let tags = checkForTags(msg);
-        let entry = { "msg": msg, "timeStamp": time, "tags": tags};
+        let tags = utils.checkForTags(msg); //store tags
+        if (tags.length != 0) msg = utils.removeTags(msg); //remove tags if exist, only storing message
         
+
+        let entry = { "msg": msg, "timeStamp": time, "tags": tags };
+
         this.entryList.push(entry);
+    };
+
+    //Get all messages in an entry
+    entries() {
+        let allEntries = this.entryList.map(entry => entry.msg);
+        return allEntries;
+    };
+
+    //get a list of all tags used
+    tags() {
+
+        this.entryList.map(entry => {
+            entry.tags.forEach(tag => this.allTags.push(tag));
+        });
+
+
+        //Passing in allTags array to a Set constructor: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+        //All values are now unique using set and converting back to array
+        return Array.from(new Set(this.allTags));
     }
+
+    //get messages with the given tag
+    entriesWithTag(tagSearch) {
+        let entries = [];
+
+        this.entryList.forEach(entry => {
+            if (entry.tags.includes(tagSearch)) {
+                entries.push(entry.msg);
+            }
+        });
+
+
+        return entries;
+    }
+
+    date(time) {
+        time = moment(time).format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+        let entries = [];
+
+        this.entryList.forEach(entry => {
+            if (entry.timeStamp == time) {
+                entries.push(entry.msg);
+            }
+        });
+
+        return entries;
+    }
+
+    today() {
+        let today = moment().format("DD");
+        let entries = [];
+        let entryDay;
+
+        this.entryList.forEach(entry => {
+            entryDay = moment(entry.timeStamp, "dddd, MMMM Do YYYY, h:mm:ss a").format("DD");
+            if (entryDay === today) {
+                entries.push(entry.msg);
+            }
+        });
+
+        return entries;
+    }
+
+    search(keyword) {
+        let results = [];
+
+        this.entryList.forEach(entry => {
+            if (entry.msg.includes(keyword)) {
+                results.push(entry.msg);
+            }
+        });
+
+        return results;
+    }
+
 };
-
-
-function checkForTags(message) {
-    
-    let entryWords = message.trim().split(" ");
-    let tags = entryWords.filter(word => {
-        if(word.includes("#")){
-              return word.substring(1, word.length);      
-        }
-    });
-
-    return tags || [];
-}
-
 
 module.exports = Diary;
