332a72dae1a73c5bd884ece8b368b787616ed102
Aaron Saloff <aaronsaloff@gmail.com>
2017-10-15 13:54:18 -0400

    created diary module

--- diary.js
+++ diary.js
@@ -1,5 +1,98 @@
-const Diary = () => {
+const fs = require('fs');
 
+function Diary() {
+  this.data = [];
+
+  this.entries = () => {
+    return this.data;
+  };
+
+  this.entry = (message, date) => {
+    date ? date : date = Date.parse(new Date());
+
+    var tags = checkMessageForTags(message);
+
+    var newEntry = { message, date, tags };
+    this.data.push(newEntry);
+  };
+
+  this.tags = () => {
+    var tags = [];
+
+    this.entries().forEach((entry) => {
+      tags.push(entry.tags);
+    });
+
+    return [].concat.apply([], tags);
+  };
+
+  this.entriesWithTag = (tag) => {
+    var applicableEntries = [];
+
+    this.data.forEach((entry) => {
+      if (entry.tags.indexOf(tag) !== -1) {
+        applicableEntries.push(entry);
+      }
+    });
+
+    return applicableEntries;
+  };
+
+  this.date = (date) => {
+    var applicableEntries = [];
+    var targetDate = new Date(date);
+
+    this.data.forEach((entry) => {
+      var entryDate = new Date(entry.date);
+
+      if (entryDate.setHours(0,0,0,0) == targetDate.setHours(0,0,0,0)) {
+        applicableEntries.push(entry);
+      }
+    });
+
+    return applicableEntries;
+  };
+
+  this.today = () => {
+    var today = Date.parse(new Date());
+    return this.date(today);
+  };
+
+  this.search = (term) => {
+    term = term.toLowerCase();
+    var applicableEntries = [];
+
+    this.data.forEach((entry) => {
+      message = entry.message.toLowerCase();
+      if (message.indexOf(term) !== -1) {
+        applicableEntries.push(entry);
+      }
+    });
+
+    return applicableEntries;
+  };
+
+  this.save = () => {
+    var jsonString = JSON.stringify({entries: this.data}, null, 2);
+    fs.writeFileSync('./diary.json', jsonString);
+  };
+
+  this.load = () => {
+    var file = JSON.parse(fs.readFileSync('./diary.json', 'utf8'));
+    this.data = file.entries;
+  };
+}
+
+var checkMessageForTags = (message) => {
+  var regex = /#([\w]+)/g;
+  var results = [];
+
+  while(tag = regex.exec(message)) {
+    results.push(tag[1]);
+  }
+  return results;
 };
+
+
 
 module.exports = Diary;
--- spec/diarySpec.js
+++ spec/diarySpec.js
@@ -2,95 +2,109 @@ const Diary = require('../diary');
 const fs = require('fs');
 const { displayDate } = require('../helpers/date_helper');
 
-beforeEach(() => {
-  var diary = new Diary();
-});
+describe('Diary', () => {
+  var diary;
 
-describe('.entries', () => {
-  xit('returns a list of all entries', () => {
-    diary.entry('Hello diary!');
-    expect(diary.entries().length).toEqual(1);
-  });
-});
-
-describe('.entry', () => {
-  xit('adds an entry', () => {
-    diary.entry('Hello diary!');
-    expect(diary.entries()[0].message).toEqual('Hello diary!');
+  beforeEach(() => {
+    diary = new Diary();
   });
 
-  xit("adds an entry with the time/date of it's creation", () => {
-    diary.entry('Hello diary!');
-    expect(displayDate(diary.entries()[0].date)).toEqual(displayDate(Date.parse(new Date())));
+  afterEach(() => {
+    fs.writeFileSync('./diary.json', '');
   });
 
-  xit("allows for a date to be added manually", () => {
-    var date = Date.parse("Mon, 25 Dec 1995 13:30:00 GMT");
-    diary.entry('Hello diary!', date);
-    expect(diary.entries()[0].date).toEqual(date);
+  describe('.entries', () => {
+    it('returns a list of all entries', () => {
+      diary.entry('Hello diary!');
+      expect(diary.entries().length).toEqual(1);
+    });
   });
 
-  xit("saves the tags written in the entry", () => {
-    diary.entry('Hello diary! #hello #there');
-    expect(diary.entries()[0].tags).toEqual(['hello', 'there']);
-  });
-});
+  describe('.entry', () => {
+    it('adds an entry', () => {
+      diary.entry('Hello diary!');
+      expect(diary.entries()[0].message).toEqual('Hello diary!');
+    });
 
-describe('.tags', () => {
-  xit('returns a list of all tags entered in all entries', () => {
-    diary.entry('Hello diary! #hello #there');
-    diary.entry('Goodbye diary! #seeyou');
-    expect(diary.tags()).toEqual(['hello', 'there', 'seeyou']);
-  });
-});
+    it("adds an entry with the time/date of it's creation", () => {
+      diary.entry('Hello diary!');
+      expect(displayDate(diary.entries()[0].date)).toEqual(displayDate(Date.parse(new Date())));
+    });
 
-describe('.entriesWithTag', () => {
-  xit('returns a list of all entries with the given tag', () => {
-    var entry1 = diary.entry('Hello diary! #hello #there');
-    diary.entry('Goodbye diary! #seeyou');
-    expect(diary.entriesWithTag('hello')).toEqual([entry1]);
-  });
-});
+    it("allows for a date to be added manually", () => {
+      var date = Date.parse("Mon, 25 Dec 1995 13:30:00 GMT");
+      diary.entry('Hello diary!', date);
+      expect(diary.entries()[0].date).toEqual(date);
+    });
 
-describe('.today', () => {
-  xit('returns a list of all entries written today', () => {
-    var entry1 = diary.entry('Hello diary! #hello #there');
-    diary.entry('Goodbye diary! #seeyou', Date.parse("Mon, 25 Dec 1995 13:30:00 GMT"));
-    expect(diary.today()).toEqual([entry1]);
+    it("saves the tags written in the entry", () => {
+      diary.entry('Hello diary! #hello #there');
+      expect(diary.entries()[0].tags).toEqual(['hello', 'there']);
+    });
   });
-});
 
-describe('.date', () => {
-  xit('returns a list of all entries on the given date', () => {
-    var entry1 = diary.entry('Hello diary! #hello #there');
-    diary.entry('Goodbye diary! #seeyou', Date.parse("Mon, 25 Dec 1995 13:30:00 GMT"));
-    expect(diary.today()).toEqual([entry1]);
+  describe('.tags', () => {
+    it('returns a list of all tags entered in all entries', () => {
+      diary.entry('Hello diary! #hello #there');
+      diary.entry('Goodbye diary! #seeyou');
+      expect(diary.tags()).toEqual(['hello', 'there', 'seeyou']);
+    });
   });
-});
 
-describe('.search', () => {
-  xit('returns a list of all entries with the given search term', () => {
-    var entry1 = diary.entry('Hello diary!');
-    diary.entry('Goodbye diary!');
-    expect(diary.search('Hello')).toEqual([entry1]);
+  describe('.entriesWithTag', () => {
+    it('returns a list of all entries with the given tag', () => {
+      diary.entry('Hello diary! #hello #there');
+      diary.entry('Goodbye diary! #seeyou');
+      expect(diary.entriesWithTag('hello')).toEqual([diary.entries()[0]]);
+    });
   });
-});
 
-describe('.save', () => {
-  xit('saves all diary entries to the JSON file', () => {
-    diary.entry('Goodbye diary!', Date.parse('5/5/2015'));
-    diary.save();
-    var expectedJson = { entries: [ { message: 'Goodbye diary!', date: 1430798400000, tags: [] } ] };
-    var file = fs.readFileSync('../diary.json', 'utf8');
-    var jsonFile = JSON.parse(file);
-    expect(jsonFile).toEqual(expectedJson);
+  describe('.today', () => {
+    it('returns a list of all entries written today', () => {
+      diary.entry('Hello diary! #hello #there');
+      diary.entry('Goodbye diary! #seeyou', Date.parse("Mon, 25 Dec 1995 13:30:00 GMT"));
+      expect(diary.today()).toEqual([diary.entries()[0]]);
+    });
   });
-});
 
-describe('.load', () => {
-  xit('loads all diary entries to the JSON file', () => {
-    fs.writeFileSync(JSON.stringify({ entries: [ { message: 'Goodbye diary!', date: 1430798400000, tags: [] } ] }, null, 2));
-    diary.load();
-    expect(diary.entries()).toEqual([ { message: 'Goodbye diary!', date: 1430798400000, tags: [] } ]);
+  describe('.date', () => {
+    it('returns a list of all entries on the given date', () => {
+      diary.entry('Hello diary! #hello #there');
+      diary.entry('Goodbye diary! #seeyou', Date.parse("Mon, 25 Dec 1995 13:30:00 GMT"));
+      expect(diary.date(Date.parse("Mon, 25 Dec 1995 13:30:00 GMT"))).toEqual([diary.entries()[1]]);
+    });
+  });
+
+  describe('.search', () => {
+    it('returns a list of all entries with the given search term', () => {
+      diary.entry('Hello diary!');
+      diary.entry('Goodbye diary!');
+      expect(diary.search('Hello')).toEqual([diary.entries()[0]]);
+    });
+
+    it('returns the case insensitive list with the given search term', () => {
+      diary.entry('Hello diary!');
+      diary.entry('Goodbye diary!');
+      expect(diary.search('hello')).toEqual([diary.entries()[0]]);
+    });
+  });
+
+  describe('.save', () => {
+    it('saves all diary entries to the JSON file', () => {
+      diary.entry('Goodbye diary!', Date.parse('5/5/2015'));
+      diary.save();
+      var expectedJson = { entries: [ { message: 'Goodbye diary!', date: 1430798400000, tags: [] } ] };
+      var file = fs.readFileSync('./diary.json', 'utf8');
+      var jsonFile = JSON.parse(file);
+      expect(jsonFile).toEqual(expectedJson);
+    });
+  });
+
+  describe('.load', () => {
+    it('loads all diary entries to the JSON file', () => {
+      fs.writeFileSync('./diary.json', JSON.stringify({ entries: [ { message: 'Goodbye diary!', date: 1430798400000, tags: [] } ] }, null, 2));
+      diary.load();
+      expect(diary.entries()).toEqual([ { message: 'Goodbye diary!', date: 1430798400000, tags: [] } ]);
+    });
   });
 });
