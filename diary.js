d044942566d4943a3dea521de2c31422c5c6f6cd
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-27 16:21:44 -0600

    timer switch

--- diary.js
+++ diary.js
@@ -1,16 +1,42 @@
 class Diary {
   constructor() {
     this.entryArr = [];
+    this.tagArray = [];
   }
 
   entry(str, date) {
     let entryObj = {};
+    entryObj.entryTag = '';
     entryObj.body = str;
     if (date) entryObj.date = date;
     else {
       entryObj.date = Date.parse(Date());
     }
+    let splitArray;
+    if (/\#/.test(entryObj.body)) {
+      splitArray = entryObj.body.split(' ');
+      splitArray.map(el => {
+        if (el[0] === '#') {
+          entryObj.entryTag = el;
+          if (!this.tagArray.includes(el)) {
+            this.tagArray.push(el.substring(1));
+          }
+        }
+      });
+    }
     this.entryArr.push(entryObj);
+  }
+  tags() {
+    return this.tagArray;
+  }
+  entriesWithTag(string) {
+    let resultArray = [];
+    this.entryArr.map(el => {
+      if (string === el.entryTag) {
+        resultArray.push(el.body);
+      }
+    });
+    return resultArray;
   }
 }
 
--- spec/diarySpec.js
+++ spec/diarySpec.js
@@ -1,23 +1,49 @@
-const Diary = require("../diary.js");
-
-describe("diaryEntries", () => {
-  it("returns an entry", () => {
-    const results = "stuff";
-    const date = Date.parse("Mon, 25 Dec 1995 13:30:00 GMT");
-    let diary = new Diary();
-    diary.entry(results, date);
-    expect(diary.entryArr[0].date.toString()).toMatch(date.toString());
+const Diary = require('../diary.js');
+describe('diaryMega', () => {
+  let diary;
+  beforeEach(() => {
+    diary = new Diary();
   });
+  describe('diaryEntries', () => {
+    it('returns an entry', () => {
+      const results = 'stuff';
+      const date = Date.parse('Mon, 25 Dec 1995 13:30:00 GMT');
+      diary.entry(results, date);
+      expect(
+        [diary.entryArr[0].body, diary.entryArr[0].date.toString()].toString(),
+      ).toMatch([results, date.toString()].toString());
+    });
+  });
+
+  describe('diaryTags', () => {
+    it('set array of tag names', () => {
+      let tags = [];
+      let results = ['firstTag', 'secondTag'];
+      diary.entry('#firstTag');
+      diary.entry('#secondTag');
+      tags = diary.tags();
+      expect(results.toString()).toMatch(tags.toString());
+    });
+  });
+
+  describe('diaryEntriesWithTags', () => {
+    it('return an array of tagged entries', () => {
+      diary.entry('this will not match');
+      diary.entry('this should #yolo');
+      diary.entry('this also should not');
+      diary.entry('4th time is the charm #yolo');
+      results = ['this should #yolo', '4th time is the charm #yolo'];
+      expect(results.toString()).toMatch(
+        diary.entriesWithTag('#yolo').toString(),
+      );
+    });
+  });
+
+  describe('diaryToday', () => {});
+
+  describe('diaryDate', () => {});
+
+  describe('diarySearch', () => {});
+
+  describe('diaryCLI', () => {});
 });
-
-describe("diaryTags", () => {});
-
-describe("diaryEntriesWithTags", () => {});
-
-describe("diaryToday", () => {});
-
-describe("diaryDate", () => {});
-
-describe("diarySearch", () => {});
-
-describe("diaryCLI", () => {});
