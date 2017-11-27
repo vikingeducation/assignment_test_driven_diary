6e614a8a8e769a6c78b120d66f8214b913d9f4a6
Elias Hantula <ehantula@indiana.edu>
2017-11-27 17:54:03 -0500

    dates

--- diary.js
+++ diary.js
@@ -3,22 +3,18 @@ constructor(){
 	this.diary = {};
 }
 
-entry(str){
-
+entry(str, date){
 let len = Object.keys(this.diary).length || 0
-
-this.diary["entry" + len] = str;
-
-return str;
+let currentTime = new Date();
+if (date) {
+this.diary[date] = str;
+} else {
+this.diary[currentTime] = str;
+}
+return [str,date];
 }
 
 }
-
-
-
-
-
-
 
 
 module.exports = Diary;
--- diary.spec.js
+++ diary.spec.js
@@ -7,8 +7,20 @@ it('adds entry to diary', () => {
 expect(diary.entry("Brad is everything to me.")).toEqual('Brad is everything to me.');
 });
 
+it('contains time/date of its creation', () => {
+expect(diary.entry("<3 Braaaad <3")).toEqual('<3 Braaaad <3' + diary[this.currentTime]);
+});
+
+
+it('may take an optional date argument which will be set as its time of creation', () =>{
+expect(diary.entry("dkeidk", Date.parse("Mon, 25 Dec 1995 13:30:00 GMT"))).toEqual(['dkeidk', 819898200000]);
 
 
 })
+
+});
+
+
+
 
 console.log(typeof(diary))
