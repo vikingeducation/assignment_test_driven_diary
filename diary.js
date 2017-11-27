c3af91ca0f98407d8545ee8aaba7497ab4b5082e
edwinyung <edwinyung@github.com>
2017-11-27 17:59:24 -0500

    finished before CLI

--- diary.js
+++ diary.js
@@ -6,7 +6,7 @@ class Diary {
 
   entry(str, date) {
     let entryObj = {};
-    entryObj.entryTag = '';
+    entryObj.entryTag = "";
     entryObj.body = str;
     if (date) entryObj.date = date;
     else {
@@ -14,9 +14,9 @@ class Diary {
     }
     let splitArray;
     if (/\#/.test(entryObj.body)) {
-      splitArray = entryObj.body.split(' ');
+      splitArray = entryObj.body.split(" ");
       splitArray.map(el => {
-        if (el[0] === '#') {
+        if (el[0] === "#") {
           entryObj.entryTag = el;
           if (!this.tagArray.includes(el)) {
             this.tagArray.push(el.substring(1));
@@ -37,6 +37,36 @@ class Diary {
       }
     });
     return resultArray;
+  }
+  today() {
+    let tempArray = [];
+    this.entryArr.map(el => {
+      let day = new Date();
+      let elDay = new Date(el.date);
+      if (
+        day.getDate() === elDay.getDate() &&
+        day.getMonth() === elDay.getMonth() &&
+        day.getYear() === elDay.getYear()
+      ) {
+        tempArray.push(el.body);
+      }
+    });
+    return tempArray;
+  }
+  date(input) {
+    let date = new Date(input);
+    let dateArray = [];
+    this.entryArr.map(el => {
+      let elDay = new Date(el.date);
+      if (
+        date.getDate() === elDay.getDate() &&
+        date.getMonth() === elDay.getMonth() &&
+        date.getYear() === elDay.getYear()
+      ) {
+        dateArray.push(el.body);
+      }
+    });
+    return dateArray;
   }
 }
 
--- spec/diarySpec.js
+++ spec/diarySpec.js
@@ -1,49 +1,67 @@
-const Diary = require('../diary.js');
-describe('diaryMega', () => {
+const Diary = require("../diary.js");
+describe("diaryMega", () => {
   let diary;
   beforeEach(() => {
     diary = new Diary();
   });
-  describe('diaryEntries', () => {
-    it('returns an entry', () => {
-      const results = 'stuff';
-      const date = Date.parse('Mon, 25 Dec 1995 13:30:00 GMT');
+  describe("diaryEntries", () => {
+    it("returns an entry", () => {
+      const results = "stuff";
+      const date = Date.parse("Mon, 25 Dec 1995 13:30:00 GMT");
       diary.entry(results, date);
       expect(
-        [diary.entryArr[0].body, diary.entryArr[0].date.toString()].toString(),
+        [diary.entryArr[0].body, diary.entryArr[0].date.toString()].toString()
       ).toMatch([results, date.toString()].toString());
     });
   });
 
-  describe('diaryTags', () => {
-    it('set array of tag names', () => {
+  describe("diaryTags", () => {
+    it("set array of tag names", () => {
       let tags = [];
-      let results = ['firstTag', 'secondTag'];
-      diary.entry('#firstTag');
-      diary.entry('#secondTag');
+      let results = ["firstTag", "secondTag"];
+      diary.entry("#firstTag");
+      diary.entry("#secondTag");
       tags = diary.tags();
       expect(results.toString()).toMatch(tags.toString());
     });
   });
 
-  describe('diaryEntriesWithTags', () => {
-    it('return an array of tagged entries', () => {
-      diary.entry('this will not match');
-      diary.entry('this should #yolo');
-      diary.entry('this also should not');
-      diary.entry('4th time is the charm #yolo');
-      results = ['this should #yolo', '4th time is the charm #yolo'];
+  describe("diaryEntriesWithTags", () => {
+    it("return an array of tagged entries", () => {
+      diary.entry("this will not match");
+      diary.entry("this should #yolo");
+      diary.entry("this also should not");
+      diary.entry("4th time is the charm #yolo");
+      results = ["this should #yolo", "4th time is the charm #yolo"];
       expect(results.toString()).toMatch(
-        diary.entriesWithTag('#yolo').toString(),
+        diary.entriesWithTag("#yolo").toString()
       );
     });
   });
 
-  describe('diaryToday', () => {});
+  describe("diaryToday", () => {
+    it("return an array of all entries written today", () => {
+      diary.entry("one", Date.parse("Mon, 25 Dec 1995 13:30:00 GMT"));
+      diary.entry("two");
+      diary.entry("three");
+      results = ["two", "three"];
+      expect(results.toString()).toMatch(diary.today().toString());
+    });
+  });
 
-  describe('diaryDate', () => {});
+  describe("diaryDate", () => {
+    it("returns a list of all entries written on the given date", () => {
+      diary.entry("one", Date.parse("10/10/10"));
+      diary.entry("two", Date.parse("11/11/11"));
+      diary.entry("three", Date.parse("12/12/12"));
+      results = ["one"];
+      expect(results.toString()).toMatch(
+        diary.date(Date.parse("10/10/10")).toString()
+      );
+    });
+  });
 
-  describe('diarySearch', () => {});
+  describe("diarySearch", () => {});
 
-  describe('diaryCLI', () => {});
+  describe("diaryCLI", () => {});
 });
