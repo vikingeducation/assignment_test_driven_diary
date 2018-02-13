97671f7a4bcd544981d99a10cfc25361066351fa
anne <richardson.ae@gmail.com>
2018-02-13 14:27:26 -0600

    WIP cli

--- modules/diary.js
+++ modules/diary.js
@@ -1,12 +1,17 @@
-var unless = require('unless')
+var fs = require('fs');
+// var cp = require('child_process');
+// const path = require('path');
+
 
 class Diary {
   constructor() {
     this.entries = []
     this.tags = []
+    this.path = './data/diary.json';
   }
 
   addEntry(body, date){
+    this.load()
     var timestamp;
     date === undefined ? timestamp = new Date() : timestamp = new Date(date);
     this.entries.push([body, timestamp])
@@ -15,14 +20,29 @@ class Diary {
     var uniqueTags = matches.filter(match => !this.tags.includes(match))
 
     this.tags = this.tags.concat(uniqueTags)
+    this.save()
+  }
+
+  listEntries(){
+    this.load()
+    console.log(this.entries);
+  }
+
+  listTags(){
+    this.load()
+    console.log(this.tags);
   }
 
   entriesWithTag(tagName){
+    this.load()
+
     var filteredEntries = this.entries.filter(function(entry){ return entry[0].includes(tagName) });
     return filteredEntries
   }
 
   today(){
+    this.load()
+
     var _today = new Date().toDateString()
     var todaysEntries = this.entries.filter(function(entry){ return entry[1].toDateString() === _today });
 
@@ -30,35 +50,50 @@ class Diary {
   }
 
   date(inputDate){
+    this.load()
+
     inputDate === undefined ? inputDate = new Date().toDateString() : inputDate = new Date(inputDate).toDateString()
     var filteredEntries = this.entries.filter(function(entry){ return entry[1].toDateString() === inputDate });
     return filteredEntries
   }
 
   search(input){
+    this.load()
     if (input === undefined) {
       throw new Error('Please enter a search term')
     } else {
       input = input.trim().toLowerCase()
     }
+
     var filteredEntries = this.entries.filter(function(entry){ return entry[0].toLowerCase().includes(input) });
     return filteredEntries
   }
 
-  save(dir){}
+  save() {
+    let data = { entries: this.entries, tags: this.tags };
+    data = JSON.stringify(data, null, 2);
+    return fs.writeFileSync(this.path, data);
+  }
 
-  load(dir){}
+  load() {
+    var data = fs.readFileSync(this.path, 'utf8');
+    data = JSON.parse(data)
+
+    this.entries = data.entries;
+    this.tags = data.tags;
+  }
+
 }
 
-// var diary = new Diary()
-// diary.addEntry('Mooky Mook #juno #fomo')
-// diary.addEntry("sample entry from the past", "Mon, 25 Dec 1995 13:30:00 GMT")
-// diary.search()
-// diary.date("12/25/1995")
-// console.log(diary.today.length);
-// diary.entry('zample clamp')
-
-// console.log(diary.today());
-
 module.exports = Diary
+
+
+
+var diary = new Diary()
+// diary.search('mook')
+// diary.addEntry("it's mardi gras day #tired #yolo #fomo")
+// diary.addEntry("sample entry from the past", "Mon, 25 Dec 1995 13:30:00 GMT")
+// diary.load()
+// diary.save()
+diary.listEntries()
 
